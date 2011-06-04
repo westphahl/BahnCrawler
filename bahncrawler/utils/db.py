d83dfd771f815c67969f0fd7999a1962db68e101
Simon Westphahl <westphahl@gmail.com>
2011-06-04 15:20:03 +0200

    Adds documentation for db module.

--- bahncrawler/utils/db.py
+++ bahncrawler/utils/db.py
@@ -1,13 +1,20 @@
-import MySQLdb
 import sys
 import logging
+import MySQLdb
 
 from bahncrawler.utils.conf import settings
 
 
 class Connection(object):
-    
+    """Klasse fuer Verbindung zur MySQL-Datenbank."""
+
     def __init__(self):
+        """
+        Initialisierungsmethode fuer eine neue Datenbankverbindung.
+
+        Es wird versucht eine Verbindung mit der Datenbank herzustellen.
+        Gelingt dies nicht, wird das Programm beendet.
+        """
         try:
             self.con = MySQLdb.connect(
                     settings['host'],
@@ -21,6 +28,13 @@ class Connection(object):
             sys.exit(1)
 
     def get_cursor(self):
+        """
+        Methode um einen neuen Cursor fuer die Datenbank zu erhalten.
+
+        Liefert als Rueckgabewert einen neuen Datenbank-Cursor, mit welchem
+        Abfragen ausgefuehrt werden koennen. Ist keine Datenbankverbindun
+        vorhanden wird das Programm beendet.
+        """
         try:
             return self.con.cursor()
         except MySQLdb.OperationalError, e:
@@ -28,8 +42,13 @@ class Connection(object):
             sys.exit(1)
 
     def __del__(self):
+        """
+        Methode zum Schliessen der Datenbankverbindung, wenn das Objekt
+        bei Programmende geloescht wird.
+        """
         if hasattr(self, 'con'):
             self.con.close()
 
 
+# Globale Datenbankverbindung erzeugen
 connection = Connection()

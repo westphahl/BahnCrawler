e68657b0d722d5e9c8b7c9efe55f274245a661b1
Simon Westphahl <westphahl@gmail.com>
2011-06-04 15:18:18 +0200

    Adds documentation for conf module.

--- bahncrawler/utils/conf.py
+++ bahncrawler/utils/conf.py
@@ -1,7 +1,11 @@
 class Settings(dict):
+    """
+    Dictionary um globale Einstellungen zu speichern.
+    """
 
     def set_from_namespace(self, ns):
+        """Einstellungen durch CLI Parameter setzen"""
         self.update(vars(ns))
 
-
+# Globales Settings-Objekt erzeugen
 settings = Settings()
